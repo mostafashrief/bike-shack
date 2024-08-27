72921bec028b1b720d2be6be7a66ca00d9a12261
mostafashrief <mostafa.shreif221@gmail.com>
2024-08-27 17:09:40 +0300

    new contact form edits with javascript

--- assets/contact-form-custom.js
+++ assets/contact-form-custom.js
@@ -1,1 +1,29 @@
 //logic for our custom contact form
+    const businessCheckbox = document.getElementById('businessAccount');
+    const companyDetails = document.getElementById('companyInfo');
+    const contactReasons = document.getElementById('contactReason');
+    const productName = document.getElementById('productNameField');
+    const orderNumber = document.getElementById('orderNumberField');
+
+    businessCheckbox.addEventListener('change', (event) => {
+       if (businessCheckbox.checked) {
+        companyDetails.classList.remove('hidden');
+       }
+       else{
+        companyDetails.classList.add('hidden');
+       }
+    });
+
+    contactReasons.addEventListener('change', (event) => {
+        if (contactReasons.value === 'product') {
+            productName.classList.remove('hidden');
+        }
+        else if (contactReasons.value === 'order') {
+            orderNumber.classList.remove('hidden');
+        }
+    });
+    
+
+
+   
+
