228658a99088009ed9d636a5742a464fc827ef62
mostafashrief <mostafa.shreif221@gmail.com>
2024-09-17 06:47:26 +0300

    Insialize all slider seeting and dynmic data

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -1,28 +1,22 @@
-// initialize slideshow
-
-// config:
-// assume there is just one single slider on the page
-// navigation dots should be disabled
-// no autoplay
-// infinite loop
-// the slides or cells should be aligned to the left side on initialization.
-// pauseAutoPlayOnHover: false
-
-// Wait for the DOM to fully load before initializing the slider
 document.addEventListener("DOMContentLoaded", function() {
-    // Initialize the slider for the testimonial section
-    const slideshowContainer = document.querySelector(".testimonial-slider");
+
+  const slideshowContainers = document.querySelectorAll(".testimonial-slider");
+
+  slideshowContainers.forEach((slideshowContainer) => {
   
-    if (slideshowContainer) {
-      const slider = new Flickity(slideshowContainer, {
-        // Slider options
-        cellAlign: "left", // Align slides to the left
-        wrapAround: true, // Infinite loop
-        autoPlay: false, // Disable autoplay
-        pageDots: false, // Disable navigation dots
-        pauseAutoPlayOnHover: false, // Don't pause on hover
-        pageDots: false,
-      });
+    let autoplayValue = slideshowContainer.getAttribute("data-autoplay");
+    if (autoplayValue === "false") {
+      autoplayValue = false; 
+    } else {
+      autoplayValue = parseInt(autoplayValue, 10); 
     }
+
+    const slider = new Flickity(slideshowContainer, {
+      cellAlign: "left", 
+      wrapAround: true, 
+      autoPlay: autoplayValue, 
+      pageDots: false, 
+      pauseAutoPlayOnHover: false, 
+    });
   });
-  
+});
